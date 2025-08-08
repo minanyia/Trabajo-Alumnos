2adcb5c99fd071057cfa137b4c04f10999f95bf9
USER <USER@User3137-PC>
2025-08-07 20:10:50 -0600

    Realice el modelo alumno del paquete modelo

--- src/modelo/Alumno.java
+++ src/modelo/Alumno.java
@@ -1,13 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo;
 
-/**
- *
- * @author marye
- */
 public class Alumno {
-    
+
+    private String nombre;
+    private int edad;
+    private String carrera;
+    private int cuatrimestre;
+    private double promedio;
+
+    public Alumno(String nombre, int edad, String carrera, int cuatrimestre, double promedio) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.carrera = carrera;
+        this.cuatrimestre = cuatrimestre;
+        this.promedio = promedio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public int getCuatrimestre() {
+        return cuatrimestre;
+    }
+
+    public double getPromedio() {
+        return promedio;
+    }
+
 }
