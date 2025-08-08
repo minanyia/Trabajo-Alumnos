53bd8b452458688cb53edaf09844ea6ce5543013
JuPa-png <juancortes0478@gmail.com>
2025-08-07 20:18:53 -0600

    Me toco realizar el controlador

--- src/controlador/ControladorAlumno.java
+++ src/controlador/ControladorAlumno.java
@@ -4,10 +4,61 @@
  */
 package controlador;
 
+import modelo.Alumno;
+
 /**
  *
  * @author marye
  */
 public class ControladorAlumno {
+    public class GrupoController {
+ 
+    public double calcularPromedioFila(Alumno[] fila) {
+        double suma = 0;
+        for (Alumno a : fila) {
+            suma += a.getPromedio();
+        }
+        return suma / fila.length;
+    }
+ 
+    public double calcularPromedioGrupal(Alumno[][] grupo) {
+        double suma = 0;
+        int total = 0;
+        for (Alumno[] fila : grupo) {
+            for (Alumno a : fila) {
+                suma += a.getEdad();
+                total++;
+            }
+        }
+        return suma / total;
+    }
+ 
+    public int filaMasDestacada(Alumno[][] grupo) {
+        double maxProm = 0;
+        int filaDestacada = -1;
+        for (int i = 0; i < grupo.length; i++) {
+            double promFila = calcularPromedioFila(grupo[i]);
+            if (promFila > maxProm) {
+                maxProm = promFila;
+                filaDestacada = i;
+            }
+        }
+        return filaDestacada;
+    }
+ 
+    public Alumno alumnoMasDestacado(Alumno[][] grupo) {
+        Alumno mejor = null;
+        double maxProm = 0;
+        for (Alumno[] fila : grupo) {
+            for (Alumno a : fila) {
+                if (a.getPromedio() > maxProm) {
+                    maxProm = a.getPromedio();
+                    mejor = a;
+                }
+            }
+        }
+        return mejor;
+    }
+}
     
 }
