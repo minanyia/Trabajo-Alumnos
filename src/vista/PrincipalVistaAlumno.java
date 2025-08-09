465439a812997931ab4cceac8dfbae28f99efe76
KarenC8 <karencruces33@gmail.com>
2025-08-08 18:32:04 -0600

    estandarise la clase PrincipalVistaAlumno

--- src/vista/PrincipalVistaAlumno.java
+++ src/vista/PrincipalVistaAlumno.java
@@ -1,22 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package vista;
 
 import controlador.GrupoController;
 import java.util.Scanner;
 import modelo.Alumno;
 
-/**
- *
- * @author marye
- */
+
 public class PrincipalVistaAlumno {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
         GrupoController con = new GrupoController();
