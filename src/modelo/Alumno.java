79db968c5fb59dceca2602e4c53cc2a283ed0dd6
USER <karenC8>
2025-08-08 18:09:12 -0600

    re organize la clase alumno (modelo)

--- src/modelo/Alumno.java
+++ src/modelo/Alumno.java
@@ -1,21 +1,16 @@
-package modelo;
+package modelo; 
 
+// Clase que representa el modelo de un alumno
 public class Alumno {
 
+    // Atributos privados
     private String nombre;
     private int edad;
     private String carrera;
     private int cuatrimestre;
     private double promedio;
 
-    public Alumno(String nombre, int edad, String carrera, int cuatrimestre, double promedio) {
-        this.nombre = nombre;
-        this.edad = edad;
-        this.carrera = carrera;
-        this.cuatrimestre = cuatrimestre;
-        this.promedio = promedio;
-    }
-
+    // Métodos getter para acceder a los atributos privados
     public String getNombre() {
         return nombre;
     }
@@ -36,4 +31,12 @@ public class Alumno {
         return promedio;
     }
 
+    // Constructor que inicializa los atributos del alumno
+    public Alumno(String nombre, int edad, String carrera, int cuatrimestre, double promedio) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.carrera = carrera;
+        this.cuatrimestre = cuatrimestre;
+        this.promedio = promedio;
+    }
 }
