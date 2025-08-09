43c496a4d0151f577850018d8d3cf4c9090bda61
KarenC8 <karencruces33@gmail.com>
2025-08-08 18:37:56 -0600

    Limpieza de código: corrección de sangrías y eliminación de comentarios innecesarios

--- src/controlador/ControladorAlumno.java
+++ src/controlador/ControladorAlumno.java
@@ -1,64 +1,57 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controlador;
 
 import modelo.Alumno;
 
-/**
- *
- * @author marye
- */
 public class ControladorAlumno {
+
     public class GrupoController {
- 
-    public double calcularPromedioFila(Alumno[] fila) {
-        double suma = 0;
-        for (Alumno a : fila) {
-            suma += a.getPromedio();
-        }
-        return suma / fila.length;
-    }
- 
-    public double calcularPromedioGrupal(Alumno[][] grupo) {
-        double suma = 0;
-        int total = 0;
-        for (Alumno[] fila : grupo) {
+
+        public double calcularPromedioFila(Alumno[] fila) {
+            double suma = 0;
             for (Alumno a : fila) {
-                suma += a.getEdad();
-                total++;
+                suma += a.getPromedio();
             }
+            return suma / fila.length;
         }
-        return suma / total;
-    }
- 
-    public int filaMasDestacada(Alumno[][] grupo) {
-        double maxProm = 0;
-        int filaDestacada = -1;
-        for (int i = 0; i < grupo.length; i++) {
-            double promFila = calcularPromedioFila(grupo[i]);
-            if (promFila > maxProm) {
-                maxProm = promFila;
-                filaDestacada = i;
-            }
-        }
-        return filaDestacada;
-    }
- 
-    public Alumno alumnoMasDestacado(Alumno[][] grupo) {
-        Alumno mejor = null;
-        double maxProm = 0;
-        for (Alumno[] fila : grupo) {
-            for (Alumno a : fila) {
-                if (a.getPromedio() > maxProm) {
-                    maxProm = a.getPromedio();
-                    mejor = a;
+
+        public double calcularPromedioGrupal(Alumno[][] grupo) {
+            double suma = 0;
+            int total = 0;
+            for (Alumno[] fila : grupo) {
+                for (Alumno a : fila) {
+                    suma += a.getEdad();
+                    total++;
                 }
             }
+            return suma / total;
         }
-        return mejor;
+
+        public int filaMasDestacada(Alumno[][] grupo) {
+            double maxProm = 0;
+            int filaDestacada = -1;
+            for (int i = 0; i < grupo.length; i++) {
+                double promFila = calcularPromedioFila(grupo[i]);
+                if (promFila > maxProm) {
+                    maxProm = promFila;
+                    filaDestacada = i;
+                }
+            }
+            return filaDestacada;
+        }
+
+        public Alumno alumnoMasDestacado(Alumno[][] grupo) {
+            Alumno mejor = null;
+            double maxProm = 0;
+            for (Alumno[] fila : grupo) {
+                for (Alumno a : fila) {
+                    if (a.getPromedio() > maxProm) {
+                        maxProm = a.getPromedio();
+                        mejor = a;
+                    }
+                }
+            }
+            return mejor;
+        }
     }
-}
-    
+
 }
